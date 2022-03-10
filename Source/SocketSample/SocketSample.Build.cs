7d699d61b164758abeba5a6d0bdfa25534867549
junios <junios@junios.net>
2022-03-10 18:36:29 +0900

    추가 경로 설정 설정

--- Source/SocketSample/SocketSample.Build.cs
+++ Source/SocketSample/SocketSample.Build.cs
@@ -12,6 +12,7 @@ public class SocketSample : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Networking", "Sockets" });
 
 
+		//https://docs.unrealengine.com/4.27/en-US/ProductionPipelines/BuildTools/UnrealBuildTool/ThirdPartyLibraries/
 		// Add any include paths for the plugin
 		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "flatbuffer/include"));
 	}
